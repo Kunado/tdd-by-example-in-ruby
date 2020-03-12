12a23287bd61a6c73bd2e568c1bd1bc6b3b68902
naoki saruta <katsusand1@gmail.com>
2020-03-12 23:42:40 +0900

    Remove subclasses

--- lib/money.rb
+++ lib/money.rb
@@ -3,37 +3,24 @@ class Money
 
   class << self
     def doller(amount)
-      Money::Doller.new(amount, 'USD')
+      Money.new(amount, 'USD')
     end
 
     def franc(amount)
-      Money::Franc.new(amount, 'CHF')
+      Money.new(amount, 'CHF')
     end
+  end
+
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
   end
 
   def ==(other)
-    amount == other.amount && self.class == other.class
+    amount == other.amount && currency == other.currency
   end
 
-  class Doller < Money
-    def initialize(amount, currency)
-      @amount = amount
-      @currency = currency
-    end
-
-    def times(multiplier)
-      Money.doller(amount * multiplier)
-    end
-  end
-
-  class Franc < Money
-    def initialize(amount, currency)
-      @amount = amount
-      @currency = currency
-    end
-
-    def times(multiplier)
-      Money.franc(amount * multiplier)
-    end
+  def times(multiplier)
+    Money.new(amount * multiplier, currency)
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,60 +1,38 @@
 require_relative '../lib/money'
 
 describe Money do
-  describe Money::Doller do
-    context 'with 5 doller object' do
-      let(:five_doller) { Money.doller(5) }
-      describe '#times' do
-        context 'when multiplied by 2' do
-          it { expect(five_doller.times(2)).to eq Money.doller(10) }
-        end
-        context 'when multiplied by 3' do
-          it { expect(five_doller.times(3)).to eq Money.doller(15) }
-        end
+  context 'with 5 doller object' do
+    let(:five_doller) { Money.doller(5) }
+    describe '#times' do
+      context 'when multiplied by 2' do
+        it { expect(five_doller.times(2)).to eq Money.doller(10) }
       end
+      context 'when multiplied by 3' do
+        it { expect(five_doller.times(3)).to eq Money.doller(15) }
+      end
+    end
 
-      describe '#equals' do
-        context 'with another object whose amount is 5' do
-          it { expect(five_doller).to eq Money.doller(5) }
-        end
-        context 'with another object whose amount is 6' do
-          it { expect(five_doller).to_not eq Money.doller(6) }
-        end
-        context 'with another franc object whose amount is 5' do
-          it { expect(five_doller).to_not eq Money.franc(5) }
-        end
+    describe '#equals' do
+      context 'with another object whose amount is 5' do
+        it { expect(five_doller).to eq Money.doller(5) }
       end
+      context 'with another object whose amount is 6' do
+        it { expect(five_doller).to_not eq Money.doller(6) }
+      end
+      context 'with another franc object whose amount is 5' do
+        it { expect(five_doller).to_not eq Money.franc(5) }
+      end
+    end
 
-      describe '#currency' do
-        it { expect(five_doller.currency).to eq 'USD' }
-      end
+    describe '#currency' do
+      it { expect(five_doller.currency).to eq 'USD' }
     end
   end
 
-  describe Money::Franc do
-    context 'with 5 franc object' do
-      let(:five_franc) { Money.franc(5) }
-      describe '#times' do
-        context 'when multiplied by 2' do
-          it { expect(five_franc.times(2)).to eq Money.franc(10) }
-        end
-        context 'when multiplied by 3' do
-          it { expect(five_franc.times(3)).to eq Money.franc(15) }
-        end
-      end
-
-      describe '#equals' do
-        context 'with another franc object whose amount is 5' do
-          it { expect(five_franc).to eq Money.franc(5) }
-        end
-        context 'with another franc object whose amount is 6' do
-          it { expect(five_franc).to_not eq Money.franc(6) }
-        end
-      end
-
-      describe '#currency' do
-        it { expect(five_franc.currency).to eq 'CHF' }
-      end
+  context 'with 5 franc object' do
+    let(:five_franc) { Money.franc(5) }
+    describe '#currency' do
+      it { expect(five_franc.currency).to eq 'CHF' }
     end
   end
 end
